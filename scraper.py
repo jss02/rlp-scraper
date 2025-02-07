802ca135d54792dc821b46c4fce49d20785c599f
jss02 <jjjjjssss0326@gmail.com>
2025-02-08 00:49:51 +1100

    Implementing scrape_game funcitonality

--- scraper.py
+++ scraper.py
@@ -1,13 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_matches(url):
+
+
+def scrape_matches(url):
     rlp_url = "https://www.rugbyleagueproject.org"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    game_links = soup.find_all('a', class_='rlplnk')
-    print(game_links)
+    games = soup.find_all('a', class_='rlplnk')
+    game_links = []
+    for game in games:
+        game_links.append(rlp_url + game['href'])
+    
+    return game_links
+
+def scrape_game(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    print(soup.text)
 
 if __name__ == "__main__":
-    get_matches("https://www.rugbyleagueproject.org/seasons/nrl-2004/results.html")
+    matches = scrape_matches("https://www.rugbyleagueproject.org/seasons/nrl-2004/results.html")
+    
+    scrape_game(matches[0])
